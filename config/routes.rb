9bf962bf40824056774ef1011456a81b5e597971
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-06-27 21:34:07 +0300

    first test and fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get 'orders', to: "orders#index"
-  get 'setting', to: "settings#index"
+  get 'settings', to: "settings#index"
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
