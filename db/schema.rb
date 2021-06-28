1f2d2ae502a58e65b6156f547318e1fa50b11692
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-06-28 17:05:19 +0300

    references to review: user and book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_135448) do
+ActiveRecord::Schema.define(version: 2021_06_28_140421) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 2021_06_28_135448) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id", null: false
+    t.integer "user_id", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,4 +72,6 @@ ActiveRecord::Schema.define(version: 2021_06_28_135448) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
