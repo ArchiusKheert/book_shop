5454d9813eb04b239b952f37decf95143ebf0fa8
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-06-28 14:54:58 +0300

    FB Auth Update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,7 +274,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   #
   config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'],
-                     scope: 'email'
+                     scope: 'email,user_friends,user_birthday,user_location'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
