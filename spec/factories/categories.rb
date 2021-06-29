89997f409108e292a91458ae33df30bf65048298
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-06-29 14:33:55 +0300

    review controller and test checking

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :category do
-    name { "MyString" }
+    name { "Category Name" }
+  end
+
+  factory :category_invalid do
+    name nil
+  end
+
+  factory :category_updated do
+    name "Updated category name"
   end
 end
