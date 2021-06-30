567892b6910e56d9f2f73e10dd60f619e1a8d127
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-06-30 13:33:37 +0300

    description slicing and new modules for book and authors

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,10 @@
 module BooksHelper
+  def book_short_description(book, size =50)
+    return book.description if book.description.length <= 50
+    book.description.slice(0,size-3).concat('...')
+  end
+
+  def book_authors
+    book.authors.map { |aut| author_name(aut) }.join(', ')
+  end
 end
