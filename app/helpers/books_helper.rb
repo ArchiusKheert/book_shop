bfff5bb56db1ee45564bf9edc37a3a5ecd243f99
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-07-06 21:21:28 +0300

    book show

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -7,4 +7,8 @@ module BooksHelper
   def book_authors(book)
     book.authors.map { |aut| author_name(aut) }.join(', ')
   end
+
+  def book_dimensions(book)
+    "H: #{book.height}\" x W: #{book.width}\" x D: #{book.depth}\""
+  end
 end
