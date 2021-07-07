ee99437404a850c75d1c89c9f5822818053c29bf
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-07-07 18:53:21 +0300

    prepare for pay system

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,68 +1,19 @@
 class ReviewsController < ApplicationController
 
-  before_action :set_review, only: %i[ show edit update destroy ]
 
-  # GET /reviews or /reviews.json
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1 or /reviews/1.json
-  def show
-  end
-
-  # GET /reviews/new
-  def new
-    @review = Review.new
-  end
-
-  # GET /reviews/1/edit
-  def edit
-  end
 
   # POST /reviews or /reviews.json
   def create
-    @review = Review.new(review_params)
-
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to reviews_url}
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @reviews.errors, status: :unprocessable_entity }
-      end
-    end
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.create(review_params)
+    redirect_to book_path(@book)
   end
 
-  # PATCH/PUT /reviews/1 or /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to reviews_url }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /reviews/1 or /reviews/1.json
-  def destroy
-    @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url}
-      format.json { head :no_content }
-    end
-  end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_review
-    @review = Review.find(params[:id])
-  end
 
-  # Only allow a list of trusted parameters through.
   def review_params
-    params.require(:review).permit(:title, :text, :rating)
+    params.require(:review).permit(:title, :text, :rating, :book_id, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :categories
   resources :authors
-  resources :books
-  resources :reviews
+  resources :books do
+    resources :reviews
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
