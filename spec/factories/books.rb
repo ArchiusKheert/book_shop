b1693dd9bb4938a9565c8d45365b50f3ffbda48e
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-07-31 15:48:00 +0300

    refactoring schema and seed

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :book do
     title {FFaker::Book.title}
     price {[5.99,10.99,15.99,20.99,35.99,99.99].sample}
-    quantity { 1 }
     description {FFaker::Book.description}
     height { "9.99" }
     width { "9.99" }
