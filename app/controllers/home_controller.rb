e2e39260832737383529792a7150e57c8406aa70
Anton Artemenko <AntonArtemenko324@gmail.com>
2021-12-16 17:46:03 +0200

    bestsellers adding

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    #@latest_books = Book.last(3)
-    @bestsellers = Book.last(4)
+    @latest_books = Book.last(3)
+    @bestsellers = Category.ids.map { |c_id| Book.bestsellers(c_id).first}
   end
 end
